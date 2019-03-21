2529a55b6cdc65d935fa763520f766fb19ea9114
Steven Fuller <stevenfuller@hotmail.ca>
2019-03-21 16:31:24 -0400

    Added register, log in, and log out functionality

--- index.js
+++ index.js
@@ -11,16 +11,43 @@ const Campground = require(`./models/campground`);
 const Comment = require(`./models/comment`);
 const User = require(`./models/user`);
 
+// Middleware
+const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect(`/login`);
+};
+
 // Config
 const app = express();
 mongoose.connect(`mongodb://localhost:27017/yelp_camp`, {
   useNewUrlParser: true,
 });
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  require(`express-session`)({
+    secret: `Murphy is a silly doggo`,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.set(`view engine`, `ejs`);
 
 seedDB();
 
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// Routes
 app.get(`/`, (req, res) => {
   res.render(`landing.ejs`);
 });
@@ -41,7 +68,7 @@ app.post(`/campgrounds`, (req, res) => {
     .catch(err => console.error(err));
 });
 
-app.get(`/campgrounds/new`, (req, res) => {
+app.get(`/campgrounds/new`, isLoggedIn, (req, res) => {
   res.render(`campgrounds/new`);
 });
 
@@ -54,14 +81,13 @@ app.get(`/campgrounds/:id`, (req, res) => {
 });
 
 // Comments routes
-
-app.get(`/campgrounds/:id/comments/new`, (req, res) => {
+app.get(`/campgrounds/:id/comments/new`, isLoggedIn, (req, res) => {
   Campground.findById(req.params.id)
     .then(campground => res.render(`comments/new`, { campground }))
     .catch(err => console.error(err));
 });
 
-app.post(`/campgrounds/:id/comments`, (req, res) => {
+app.post(`/campgrounds/:id/comments`, isLoggedIn, (req, res) => {
   const { text, author } = req.body;
   const newComment = { text, author };
   Comment.create(newComment)
@@ -75,6 +101,42 @@ app.post(`/campgrounds/:id/comments`, (req, res) => {
         .catch(err => console.error(err));
     })
     .catch(err => console.error(err));
+});
+
+// Auth routes
+app.get(`/register`, (req, res) => {
+  res.render(`register`);
+});
+
+app.post(`/register`, (req, res) => {
+  const newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password)
+    .then(() => {
+      passport.authenticate(`local`)(req, res, () => {
+        res.redirect(`/campgrounds`);
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      return res.render(`register`);
+    });
+});
+
+app.get(`/login`, (req, res) => {
+  res.render(`login`);
+});
+
+app.post(
+  `/login`,
+  passport.authenticate(`local`, {
+    successRedirect: `/campgrounds`,
+    failureRedirect: `/login`,
+  })
+);
+
+app.get(`/logout`, (req, res) => {
+  req.logout();
+  res.redirect(`/campgrounds`);
 });
 
 app.listen(3000, () => {
