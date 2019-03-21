fc2fa12eeab40ee1e06b9d7af95cc9f9d9392148
Steven Fuller <stevenfuller@hotmail.ca>
2019-03-21 13:20:26 -0400

    Added view engine, added container to header file, imported dependencies for auth

--- index.js
+++ index.js
@@ -1,64 +1,67 @@
 // Dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const seedDB = require('./seeds');
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const mongoose = require(`mongoose`);
+const passport = require(`passport`);
+const LocalStrategy = require(`passport-local`);
+const seedDB = require(`./seeds`);
 
 // Models
-const Campground = require('./models/campground');
-const Comment = require('./models/comment');
+const Campground = require(`./models/campground`);
+const Comment = require(`./models/comment`);
+const User = require(`./models/user`);
 
 // Config
 const app = express();
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+mongoose.connect(`mongodb://localhost:27017/yelp_camp`, {
   useNewUrlParser: true,
 });
 app.use(bodyParser.urlencoded({ extended: false }));
-app.set('view engine', 'ejs');
+app.set(`view engine`, `ejs`);
 
 seedDB();
 
-app.get('/', (req, res) => {
-  res.render('landing.ejs');
+app.get(`/`, (req, res) => {
+  res.render(`landing.ejs`);
 });
 
-app.get('/campgrounds', (req, res) => {
+app.get(`/campgrounds`, (req, res) => {
   Campground.find({})
-    .then(campgrounds => res.render('campgrounds/index', { campgrounds }))
+    .then(campgrounds => res.render(`campgrounds/index`, { campgrounds }))
     .catch(err => console.error(err));
 });
 
-app.post('/campgrounds', (req, res) => {
+app.post(`/campgrounds`, (req, res) => {
   const { name } = req.body;
   const { image } = req.body;
   const { description } = req.body;
   const newCampground = { name, image, description };
   Campground.create(newCampground)
-    .then(() => res.redirect('/campgrounds'))
+    .then(() => res.redirect(`/campgrounds`))
     .catch(err => console.error(err));
 });
 
-app.get('/campgrounds/new', (req, res) => {
-  res.render('campgrounds/new');
+app.get(`/campgrounds/new`, (req, res) => {
+  res.render(`campgrounds/new`);
 });
 
-app.get('/campgrounds/:id', (req, res) => {
+app.get(`/campgrounds/:id`, (req, res) => {
   Campground.findById(req.params.id)
-    .populate('comments')
+    .populate(`comments`)
     .exec()
-    .then(campground => res.render('campgrounds/show', { campground }))
+    .then(campground => res.render(`campgrounds/show`, { campground }))
     .catch(err => console.error(err));
 });
 
 // Comments routes
 
-app.get('/campgrounds/:id/comments/new', (req, res) => {
+app.get(`/campgrounds/:id/comments/new`, (req, res) => {
   Campground.findById(req.params.id)
-    .then(campground => res.render('comments/new', { campground }))
+    .then(campground => res.render(`comments/new`, { campground }))
     .catch(err => console.error(err));
 });
 
-app.post('/campgrounds/:id/comments', (req, res) => {
+app.post(`/campgrounds/:id/comments`, (req, res) => {
   const { text, author } = req.body;
   const newComment = { text, author };
   Comment.create(newComment)
@@ -75,5 +78,5 @@ app.post('/campgrounds/:id/comments', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('App listening on port 3000');
+  console.log(`App listening on port 3000`);
 });
